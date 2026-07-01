fd7faf97f58c88e0adff499e0e434ed51efc5aea
alwaysbangers <136464324+alwaysbangers@users.noreply.github.com>
2026-07-01 20:05:28 +0330

    Add files via upload

---
+++ database.js
@@ -0,0 +1,16 @@
+// database.js - max: 3 tags [Rec: VST + Compressor + Ozone]
+window.blogDatabase = [
+    {
+        title: "آموزش کامل کار با کمپرسور در میکس",
+        link: "https://yourblog.blogfa.com/post-123.aspx",
+        image: "https://s34.picofile.com/file/8488935000/PerfectTUNES_v2025_12_23_Win_macOS_.jpg",
+        tags: ["VST", "Compressor", "Mixing"]
+    },
+    {
+        title: "بهترین وی اس تی های سینت سایزر",
+        link: "https://yourblog.blogfa.com/post-124.aspx",
+        image: "https://s34.picofile.com/file/8488934992/Vandalism_Shocking_Ambient_Sounds_For_Spire_Spire_Presets_MiDi_ARCADiA.jpg",
+        tags: ["VST", "Sylenth1"]
+    },
+    // ... سایر پست‌ها را به همین شکل اضافه کنید
+];
