82fdd324e6b37a318e5fe31f1a2231a1d665a07c
alwaysbangers <136464324+alwaysbangers@users.noreply.github.com>
2026-07-04 18:26:02 +0330

    Add files via upload

--- database.js
+++ database.js
@@ -1,16 +1,213 @@
 // database.js - max: 3 tags [Rec: VST + Compressor + Ozone]
 window.blogDatabase = [
     {
-        title: "آموزش کامل کار با کمپرسور در میکس",
-        link: "https://yourblog.blogfa.com/post-123.aspx",
-        image: "https://s34.picofile.com/file/8488935000/PerfectTUNES_v2025_12_23_Win_macOS_.jpg",
-        tags: ["VST", "Compressor", "Mixing"]
+        title: "نرم افزار PowerISO v9.2",
+        image: "https://s34.picofile.com/file/8488348692/Power_Software_PowerISO_9_2_x86_x64_Retail_Multilingual.jpg",
+        number: "1",
+        tags: ["Software", "Utility", "ISO"]
     },
     {
-        title: "بهترین وی اس تی های سینت سایزر",
-        link: "https://yourblog.blogfa.com/post-124.aspx",
-        image: "https://s34.picofile.com/file/8488934992/Vandalism_Shocking_Ambient_Sounds_For_Spire_Spire_Presets_MiDi_ARCADiA.jpg",
-        tags: ["VST", "Sylenth1"]
+        title: "پک میدی Folk Rock EZkeys v1.0",
+        image: "https://s34.picofile.com/file/8488375818/Toontrack_Folk_Rock_EZkeys_MIDI_v1_0_0_WiNOSX_FANTASTiC.jpg",
+        number: "2",
+        tags: ["MIDI", "EZkeys", "Rock"]
     },
-    // ... سایر پست‌ها را به همین شکل اضافه کنید
+    {
+        title: "پلاگین کنترل استریو PSP stereoController2 v2.0",
+        image: "https://s34.picofile.com/file/8488377550/PSPaudioware_PSP_stereoController2_v2_0_2_R2R.jpg",
+        number: "3",
+        tags: ["VST", "Mastering", "Stereo"]
+    },
+    {
+        title: "کتاب Audio Work in Interactive Entertainment",
+        image: "https://s34.picofile.com/file/8488389476/Working_with_Sound_The_Future_of_Audio_Work_in_Interactive_Entertainment.jpg",
+        number: "4",
+        tags: ["Book", " Tutorial", "PDF"]
+    },
+    {
+        title: "پلاگین های Goodhertz All Plugins v3.13",
+        image: "https://s34.picofile.com/file/8488389518/Goodhertz_All_Plugins_Bundle_v3_13_2_Incl_Keygen_R2R.jpg",
+        number: "5",
+        tags: ["VST", "Goodhertz", "Bundle"]
+    },
+    {
+        title: "پریست تکنو Black Octopus Lunar Progressive",
+        image: "https://s34.picofile.com/file/8488418768/Lunar_Progressive_Techno_Presets_For_Serum_2_By_Imaginate.jpg",
+        number: "6",
+        tags: ["Preset", "Serum", "Techno"]
+    },
+    {
+        title: "نرم افزار Helium Music Manager v17.4",
+        image: "https://s34.picofile.com/file/8488435618/Helium_Music_Manager_Premium_v17_4_545_0.jpg",
+        number: "7",
+        tags: ["Software", "Premium", "Music"]
+    },
+    {
+        title: "کتاب Perspectives on Innovation in Music",
+        image: "https://s34.picofile.com/file/8488435634/Music_Production_Pedagogy_Perspectives_on_Innovation_in_Music_Production_Education.jpg",
+        number: "8",
+        tags: ["Book", "Tutorial", "PDF"]
+    },
+    {
+        title: "صداهای مخصوص Olly James Signature Sounds Vol.53",
+        image: "https://s34.picofile.com/file/8488435734/Olly_James_Signature_Sounds_Vol_53_Body_Mind_WAV_MiDi.jpg",
+        number: "9",
+        tags: ["Sample", "Olly James", "MIDI"]
+    },
+    {
+        title: "پریست هاوس Black Octopus Sound Grimoire",
+        image: "https://s34.picofile.com/file/8488435918/Black_Octopus_Sound_Grimoire_Witch_House_Presets_for_Serum_2_by_Tim_Schaufert.jpg",
+        number: "10",
+        tags: ["Preset", "Serum", "House"]
+    },
+    {
+        title: "درام کیت هیپ هاپ GHXSTLINE Boom Hits",
+        image: "https://s34.picofile.com/file/8488436018/WASNTMEVIELDIDIT_x_GHXSTLINE_Boom_Hits_Classic_Edition_WAV.jpg",
+        number: "11",
+        tags: ["Drum", "WAV", "Hip Hop"]
+    },
+    {
+        title: "سینتی سایزر رایگان Digital Suburban Dexed v1.0",
+        image: "https://s34.picofile.com/file/8488478068/Digital_Suburban_Dexed_v1_0_1_WiN_Mac_Linux_FREE_.jpg",
+        number: "12",
+        tags: ["VST", "Synth", "Free"]
+    },
+    {
+        title: "پلاگین پری امپ Xilentch XMTubePre",
+        image: "https://s34.picofile.com/file/8488478076/Xilentch_XMTubePre_AAXVST3AU_WIN_MAC_FREE_.jpg",
+        number: "13",
+        tags: ["VST", "Mastering", "Free"]
+    },
+    {
+        title: "پلاگین ریورب Valhalla DSP Valhalla Supermassive v5.0",
+        image: "https://s34.picofile.com/file/8488478100/Valhalla_DSP_Valhalla_Supermassive_v5_0_0_WiN_Mac_FREE_.jpg",
+        number: "14",
+        tags: ["VST", "Reverb", "Free"]
+    },
+    {
+        title: "پلاگین بانک صدا Cymatics Pandora Essentials v1.0",
+        image: "https://s34.picofile.com/file/8488478118/Cymatics_Pandora_Essentials_Rompler_v1_0_0_WiN_Mac_FREE_.jpg",
+        number: "15",
+        tags: ["VST", "Soundbank", "Free"]
+    },
+    {
+        title: "پلاگین سینتی سایزر BaconPaul Six Sines Synthesizer v1.1",
+        image: "https://s34.picofile.com/file/8488478192/BaconPaul_Six_Sines_Synthesizer_v1_1_0_WiN_Mac_Linux_FREE_.jpg",
+        number: "16",
+        tags: ["Software", "Synth", "Free"]
+    },
+    {
+        title: "پلاگین کمپرسور UnFairchild 670M mkII v1.0",
+        image: "https://s34.picofile.com/file/8488578400/UnFairchild_670M_mkII_v1_0_4_WiN_MAC_BUBBiX.jpg",
+        number: "17",
+        tags: ["VST", "Compressor", "Plugin"]
+    },
+    {
+        title: "سینتی سایزر VP-330 Vangelis Choir KONTAKT",
+        image: "https://s34.picofile.com/file/8488578434/VP_330_Vangelis_Choir_KONTAKT.jpg",
+        number: "18",
+        tags: ["Synth", "Choir", "Kontakt"]
+    },
+    {
+        title: "پلاگین کمپرسور MU-24 Comp-Limit Rev.E",
+        image: "https://s34.picofile.com/file/8488578442/MU_24_Comp_Limit_Rev_E_For_NEBULA_ARCADiA.jpg",
+        number: "19",
+        tags: ["VST", "Compressor", "Nebula"]
+    },
+    {
+        title: "پلاگین سینتی سایزر Trident Mk III v1.0",
+        image: "https://s34.picofile.com/file/8488578450/Trident_v1_0_10_84_WiN_R2R.jpg",
+        number: "20",
+        tags: ["VST", "Synth", "Keyboard"]
+    },
+    {
+        title: "وی اس تی کمپرسور Open Compressor v1.0",
+        image: "https://s34.picofile.com/file/8488578476/Open_Compressor_v1_0_2_VST3_x64_WiN_R2R.jpg",
+        number: "21",
+        tags: ["VST", "Compressor", "Plugin"]
+    },
+    {
+        title: "سینتی سایزر جالب Virta v1.9",
+        image: "https://s34.picofile.com/file/8488578518/Virta_v1_9_x_AU_VST2_x64_WiN_MAC_R2R.jpg",
+        number: "22",
+        tags: ["VST", "Synth", "Plugin"]
+    },
+    {
+        title: "مسدود کننده شبکه TEAM R2R Network Block Runtime v2.3",
+        image: "https://s34.picofile.com/file/8488578484/TEAM_R2R_Network_Block_Runtime_v2_3_0_R2R.jpg",
+        number: "23",
+        tags: ["R2R", "Runtime", "Windows"]
+    },
+    {
+        title: "سمپل سای ترنس Katayy Records Sample Pack 02",
+        image: "https://s34.picofile.com/file/8488934826/Katayy_Records_Sample_Pack_02_Psy_Trance_WAV_FANTASTiC.jpg",
+        number: "24",
+        tags: ["Sample", "Psy Trance", "WAV"]
+    },
+    {
+        title: "سینتی سایزر The Usual Suspects Nodal Red 2X",
+        image: "https://s34.picofile.com/file/8488934834/The_Usual_Suspects_Nodal_Red_2X_DSP56300_Emulator_2_0_15_WiN_Mac_Linux_ROMS_FREE_.jpg",
+        number: "25",
+        tags: ["VST", "Synth", "Free"]
+    },
+    {
+        title: "کتاب Tuning the World The Rise of 440 Hertz in Music",
+        image: "https://s34.picofile.com/file/8488934842/Tuning_the_World_The_Rise_of_440_Hertz_in_Music_Science_and_Politics_1859%E2%80%931955_by_Fanny_Gribenski_EPUBPDF.jpg",
+        number: "26",
+        tags: ["Book", "PDF", "Music"]
+    },
+    {
+        title: "ویدیوهای آموزشی Groove3 Top 10 Reverb Tips",
+        image: "https://s34.picofile.com/file/8488934926/Groove3_Top_10_Reverb_Tips_TUTORiAL_FANTASTiC.jpg",
+        number: "27",
+        tags: ["Video", "Reverb", "Tutorials"]
+    },
+    {
+        title: "فایروال اینترنت Little Snitch v6.3",
+        image: "https://s34.picofile.com/file/8488934934/Little_Snitch_6_3_3_Multilingual_macOS.jpg",
+        number: "28",
+        tags: ["Software", "Network", "Mac"]
+    },
+    {
+        title: "پلاگین شبیه ساز VZtec Fuzz v0.1",
+        image: "https://s34.picofile.com/file/8488934942/VZtec_Fuzz_v0_1_0_WiN_Mac_Linux_FREE_.jpg",
+        number: "29",
+        tags: ["VST", "Plugin", "Free"]
+    },
+    {
+        title: "پلاگین کمپرسور Cupwise 4k Crush Nebula",
+        image: "https://s34.picofile.com/file/8488934950/Cupwise_4k_crush_Nebula_Library_ARCADiA.jpg",
+        number: "30",
+        tags: ["VST", "Compressor", "Nebula"]
+    },
+    {
+        title: "نرم افزار Full Discography v1.8",
+        image: "https://s34.picofile.com/file/8488934968/Full_Discography_v1_8_7.jpg",
+        number: "31",
+        tags: ["Software", "P2P", "Music"]
+    },
+    {
+        title: "نرم افزار All Remixes v1.5",
+        image: "https://s34.picofile.com/file/8488934976/All_Remixes_v1_5_5.jpg",
+        number: "32",
+        tags: ["Software", "P2P", "Music"]
+    },
+    {
+        title: "نرم افزار Rogue Amoeba Farrago v2.1",
+        image: "https://s34.picofile.com/file/8488934984/Rogue_Amoeba_Farrago_2_1_4_macOS_TNT.jpg",
+        number: "33",
+        tags: ["Software", "TNT", "Mac"]
+    },
+    {
+        title: "پریست امبینت Vandalism Shocking Ambient Sounds",
+        image: "https://s34.picofile.com/file/8488934992/Vandalism_Shocking_Ambient_Sounds_For_Spire_Spire_Presets_MiDi_ARCADiA.jpg",
+        number: "34",
+        tags: ["Preset", "Spire", "Ambient"]
+    },
+    {
+        title: "برنامه PerfectTUNES v2025",
+        image: "https://s34.picofile.com/file/8488935000/PerfectTUNES_v2025_12_23_Win_macOS_.jpg",
+        number: "35",
+        tags: ["Software", "P2P", "Music"]
+    },
 ];
